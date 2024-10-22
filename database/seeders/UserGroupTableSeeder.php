b80e094a435eb892837eede645f66416056651e4
kaikitano0610 <k.kitano.kai@gmail.com>
2024-10-22 13:49:07 +0900

    保存し忘れ

--- database/seeders/UserGroupTableSeeder.php
+++ database/seeders/UserGroupTableSeeder.php
@@ -11,14 +11,14 @@ class UserGroupTableSeeder extends Seeder
     {
         DB::table('user_group')->insert([
             [
-                'user_id' => 1, // Admin
+                'user_id' => 2, 
                 'group_id' => 1,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'user_id' => 2, // General
-                'group_id' => 2,
+                'user_id' => 3, 
+                'group_id' => 1,
                 'created_at' => now(),
                 'updated_at' => now(),
             ]
